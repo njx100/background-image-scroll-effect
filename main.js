0b89bf65f81eac99a414bebf6ff919576fd0c986
njx100 <dnnguyen100@gmail.com>
2023-04-01 15:38:05 +0700

    completed project

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const bgImageEl = document.getElementById("bg-image");
 
+function updateImage() {
+    bgImageEl.style.opacity = .9 - (window.pageYOffset / 900);
+   
+}
+
+window.addEventListener("scroll", () => {
+    updateImage();
+});
